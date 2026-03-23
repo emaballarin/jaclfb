6f6cb4b1153b239e82fae7388a4ff7cb7758303f
Emanuele Ballarin <emanuele@ballarin.cc>
2026-03-23 17:52:12 +0100

    Rework

--- benchmark.sh
+++ benchmark.sh
@@ -9,7 +9,9 @@ time python fizzbuzz.py >> txout_py.txt
 sleep 5
 time julia fizzbuzz.jl >> txout_jl.txt
 sleep 5
-time ./cppfb.x >> txout_cpp.txt
-sleep 5
 time root -q -b fizzbuzz_root.cpp >> txout_root.txt
+sleep 5
+time ./cppfb.x >> txout_cpp.txt
+tail -n +12 txout_root.txt > txout_root_trimmed.txt
+mv txout_root_trimmed.txt txout_root.txt
 md5sum txout_py.txt txout_jl.txt txout_cpp.txt txout_root.txt
---
+++ clean.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env fish
+rm -f txout_*.txt cppfb.x
